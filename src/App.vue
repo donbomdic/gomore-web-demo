9f83eb940855a85a1cfffa1c1954df8ec98e5b3f
donbomdic <don.wong@bomdic.com>
2022-10-14 22:19:51 +0800

    fix: lang change

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,7 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import NavbarVue from "./components/NavbarVue.vue";
 import FooterVue from "./components/FooterVue.vue";
-import { ref, computed, watch } from "vue";
-import { useRouter } from "vue-router";
+import { ref } from "vue";
 
 export default {
   name: "App",
@@ -23,10 +22,8 @@ export default {
   watch: {
     $route(to, from) {
       if (["tw", "cn"].includes(window.location.pathname.slice(1, 3))) {
-        console.log("isZH");
         this.isZH = true;
       } else {
-        console.log("!isZH");
         this.isZH = false;
       }
     },
